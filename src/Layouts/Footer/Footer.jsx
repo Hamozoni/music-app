aa2737e05cf6021c89857bc94e78a3ca9d842a8e
Mohamed Yahia <m.yahya199290@gmail.com>
2024-09-14 21:48:22 +0300

    modified:   src/Layouts/Footer/Footer.jsx

--- src/Layouts/Footer/Footer.jsx
+++ src/Layouts/Footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = ()=> {
                     <h1 onClick={()=> navigate("/")}>MYH</h1>
                 </div>
                 <div className="copy-right">
-                    &copy; Copyright  { date.getFullYear() } <a href="http://www.maiyahia.com" target="_blank" rel="noopener noreferrer">Mohamed Yahia</a>  All rights reserved
+                    &copy; Copyright  { date.getFullYear() } <a href="http://www.hamozoni.com" target="_blank" rel="noopener noreferrer">Mohamed Yahia</a>  All rights reserved
                 </div>
             </div>
         </footer>
