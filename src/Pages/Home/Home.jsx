dfb5c1cccb789e687f62b2759968d5ef2c083b64
Mohamed Yahia <m.yahya199290@gmail.com>
2024-09-14 21:41:54 +0300

    modified:   src/Pages/Home/Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,14 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import ChartCarts from "../../Components/ChartCarts/ChartCarts";
 import { shazamData5 } from "../../Utils/Fetch";
 import "./Home.scss";
 import Loading from "../../Components/Loading/Loading";
-import HomeSlider from "../../Components/HomeSlider/HomeSlider";
-
-import video1 from "../../Assets/dj-1258.mp4";
 import ArtestsCard from "../../Components/ArtestsCard/ArtestsCard";
 
-import { useNavigate } from "react-router-dom";
 import Error from "../../Components/Error/Error";
 import { globalSates } from "../../Context/Context";
 
 const Home = ()=> {
-    const navigate = useNavigate()
 
     const {state} = useContext(globalSates);
 
@@ -40,25 +35,12 @@ const Home = ()=> {
 
     },[state?.listId]);
 
-    const chartVideo = useRef()
 
     return (
          error ? <Error error={error} /> :
             <div className="home">
             {isLoading ? <Loading /> :
             <>
-                <HomeSlider data={chartsData} />
-                <div className="video-player" 
-                    onClick={()=> navigate(`chart/global/all/pop/genre-global-chart-1`)}>
-                    <div className="container">
-                        <video ref={chartVideo} muted autoPlay  onEnded={()=> chartVideo.current.play()} >
-                            <source src={video1} />
-                        </video>
-                        <div className="view-chart overlay">
-                            view chart
-                        </div>
-                    </div>
-                </div>
                 <div className="container">
                     <ArtestsCard data={chartsData}/>
                     <ChartCarts title={title} data={chartsData} pFrom={"chart"}/>
